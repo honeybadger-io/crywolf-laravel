22a85b1f35663eedd369e05b399271bf532c9850
Benjamin Curtis <ben@bencurtis.com>
2018-02-01 15:58:21 -0800

    Replace Laravel 4 app with Laravel 5 app

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+    
+    public function fail()
+    {
+        return view('missing');
+    }
+}
+
+?>
