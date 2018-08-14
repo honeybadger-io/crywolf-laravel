4526231b9bfe0f997dfc2c6ee54f520a4e7a8810
Joshua Wood <josh@joshuawood.net>
2018-08-14 14:45:12 -0700

    Add /fail route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/fail', function () {
+    throw new Exception('This is a test exception raised from crywolf-laravel.');
+});
